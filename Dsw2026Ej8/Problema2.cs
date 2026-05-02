167bcc9484f7ce0c75e16d2cb634f43b3f0b9783
SantiQuiroga76 <santiquiroga.na@gmail.com>
2026-05-02 18:32:41 -0300

    Resolucion Problema 2: Clase anonima

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal totalCalculado;
+            if (quantity <= 0)
+            {
+                totalCalculado = 0;
+            }
+            else
+            {
+                totalCalculado = quantity * unitPrice;
+            }
+
+            var anonymousProduct = new {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total =  totalCalculado
+                };
+
+            return $"{anonymousProduct.Code} - {anonymousProduct.Description} - ${(anonymousProduct.Total).ToString("N2")}";
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -3,13 +3,18 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-
+        {      
             Console.WriteLine("==== PRUEBA PROBLEMA 1 =====");
             ProductHelper helper = new ProductHelper();
             String etiqueta = helper.ObtenerEtiquetaProducto(1045, "Teclado mecanico", 5764823m);
             Console.WriteLine(etiqueta);
+            Console.WriteLine();
+            Console.WriteLine("==== PRUEBA PROBLEMA 2 =====");
+            Problema2 test = new Problema2();
+            String etiquet = test.CrearResumenVenta(1765, "Parlante Bluetooth", 2, 78656m);
+            String etiquet2 = test.CrearResumenVenta(2345, "Parlante PC", -5, 756m);
+            Console.WriteLine(etiquet);
+            Console.WriteLine(etiquet2);
             Console.WriteLine();
         }
     }
